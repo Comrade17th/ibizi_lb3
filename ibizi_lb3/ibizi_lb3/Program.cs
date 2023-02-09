b8f1d3f2e9de3ac1f9c52ee816d5541bc578b9b9
nasusfen <ravshannedonat@gmail.com>
2023-02-09 09:34:46 +0300

    try

---
+++ ibizi_lb3/ibizi_lb3/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ibizi_lb3
+{
+    class Program
+    {
+        static void write(uint input)
+        {
+            string code = Convert.ToString(input, 2);
+            string res = "";
+            for(int i = 0; i < 8 - code.Length; i++)
+            {
+                res += "0";
+            }
+            res += code;
+            Console.WriteLine(res);
+        }
+        static void Main(string[] args)
+        {
+            uint a = 0b_0000_0101;
+            uint b = 0b_0000_0011;
+            uint res1 = a ^ b;
+            uint res2 = a | b;
+            uint res3 = a & b;
+            write(a);
+            write(b);
+            write(res1);
+            write(res2);
+            write(res3);
+            Console.ReadKey();
+        }
+    }
+}
---
+++ ibizi_lb3/ibizi_lb3/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// Общие сведения об этой сборке предоставляются следующим набором
+// набора атрибутов. Измените значения этих атрибутов для изменения сведений,
+// связанные с этой сборкой.
+[assembly: AssemblyTitle("ibizi_lb3")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("ibizi_lb3")]
+[assembly: AssemblyCopyright("Copyright ©  2023")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Установка значения False для параметра ComVisible делает типы в этой сборке невидимыми
+// для компонентов COM. Если необходимо обратиться к типу в этой сборке через
+// из модели COM задайте для атрибута ComVisible этого типа значение true.
+[assembly: ComVisible(false)]
+
+// Следующий GUID представляет идентификатор typelib, если этот проект доступен из модели COM
+[assembly: Guid("92222d10-2053-43d1-affb-82410c211f6b")]
+
+// Сведения о версии сборки состоят из указанных ниже четырех значений:
+//
+//      Основной номер версии
+//      Дополнительный номер версии
+//      Номер сборки
+//      Номер редакции
+//
+// Можно задать все значения или принять номера сборки и редакции по умолчанию 
+// используя "*", как показано ниже:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
